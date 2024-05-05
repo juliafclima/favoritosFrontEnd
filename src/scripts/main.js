be3aa19443d8bf500f5275e1942bd79bbe666d03
Júlia Lima <juliafclima@hotmail.com>
2024-05-05 19:51:32 -0300

    feat: first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,433 @@
+const todosLinks = {
+  /* frameworks: {
+    emoji: "🛠️",
+    data: [
+      {
+        nome: "",
+        url: "",
+        descricao: "",
+        categorias: [``],
+      },
+    ],
+  }, */
+
+  /* bibliotecas: {
+    emoji: "📚",
+    data: [
+      {
+        nome: "",
+        url: "",
+        descricao: "",
+        categorias: [``],
+      },
+    ],
+  }, */
+
+  ferramentas: {
+    emoji: "⚙️",
+    data: [
+      {
+        nome: "TransferNow",
+        url: "https://www.transfernow.net/pt",
+        descricao:
+          "Plataforma online que permite enviar arquivos grandes de forma rápida e segura.",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+    ],
+  },
+
+  fontes: {
+    emoji: "⚙️",
+    data: [
+      {
+        nome: "1001 Fonts",
+        url: "https://www.1001fonts.com/",
+        descricao: "Ampla variedade de fontes gratuitas.",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+      {
+        nome: "Abstract Fonts",
+        url: "https://www.abstractfonts.com/",
+        descricao:
+          "Extensa coleção de fontes gratuitas para uso pessoal e comercial.",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+      {
+        nome: "FontGet",
+        url: "https://www.fontget.com/",
+        descricao:
+          "Plataforma para encontrar uma variedade de fontes para seus projetos de design.",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+      {
+        nome: "FontSpace",
+        url: "https://www.fontspace.com/",
+        descricao:
+          "Vasta coleção de fontes gratuitas, é um recurso valioso para designers gráficos, desenvolvedores web e qualquer pessoa que precise de fontes para uso em seus projetos criativos.",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+      {
+        nome: "BeFonts",
+        url: "https://befonts.com/",
+        descricao:
+          "Recurso útil para encontrar uma variedade de fontes para seus projetos",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+      {
+        nome: "UrbanFonts",
+        url: "https://www.urbanfonts.com/",
+        descricao:
+          "Ótima fonte para encontrar uma variedade de fontes para seus projetos criativos",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+      {
+        nome: "Netfontes",
+        url: "https://www.netfontes.com.br/",
+        descricao:
+          "Oferece uma ampla seleção de fontes gratuitas para designers gráficos e desenvolvedores web brasileiros.",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+      {
+        nome: "Adobe Fonts",
+        url: "https://fonts.adobe.com/fonts",
+        descricao:
+          "Disponibiliza uma vasta seleção de fontes para designers, com opções premium e gratuitas.",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+      {
+        nome: "FontPair",
+        url: "https://www.fontpair.co/",
+        descricao:
+          "Plataforma que oferece sugestões de combinações de fontes para projetos de design.",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+      {
+        nome: "Colors & Fonts",
+        url: "https://www.colorsandfonts.com/google-fonts/",
+        descricao:
+          "Ferramenta que oferece uma coleção organizada de fontes do Google Fonts.",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+      {
+        nome: "WhatFontIs",
+        url: "https://www.whatfontis.com/",
+        descricao:
+          "Serviço que permite identificar fontes em imagens, fornecendo opções semelhantes e informações detalhadas.",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+      {
+        nome: "MyFonts",
+        url: "https://www.myfonts.com/",
+        descricao:
+          "Plataforma onde você pode descobrir e comprar uma grande variedade de fontes para seus projetos de design.",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+      {
+        nome: "Google Fonts",
+        url: "https://fonts.google.com/",
+        descricao:
+          "Biblioteca online que oferece uma vasta coleção de fontes gratuitas para uso em projetos criativos.",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+      {
+        nome: "Font Squirrel",
+        url: "https://www.fontsquirrel.com/",
+        descricao:
+          "Fonte confiável para baixar fontes gratuitas e de alta qualidade para seus projetos de design.",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+      {
+        nome: "Dafont",
+        url: "https://www.dafont.com/pt/",
+        descricao:
+          "Popular site onde você pode encontrar uma grande variedade de fontes gratuitas para uso em seus projetos.",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+      {
+        nome: "W3BAI",
+        url: "https://www.w3bai.com/en-US/cssref/css_websafe_fonts.html#gsc.tab=0",
+        descricao:
+          "Fonte confiável para consultar informações sobre fontes web seguras e suas propriedades CSS.",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+      {
+        nome: "CSS Font Stack",
+        url: "https://www.cssfontstack.com/",
+        descricao:
+          "Referência útil que fornece uma lista de pilhas de fontes seguras para uso em CSS.",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+    ],
+  },
+
+  banco_de_dados: {
+    emoji: "⚙️",
+    data: [
+      {
+        nome: "SQL toad",
+        url: "https://sql.toad.cz/?",
+        descricao:
+          "Ferramenta online para converter consultas SQL entre diferentes dialetos, ajudando os desenvolvedores a trabalhar com SQL de forma mais eficiente.",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+      {
+        nome: "Excalidraw",
+        url: "https://excalidraw.com/",
+        descricao:
+          "Ferramenta de desenho colaborativa baseada na web, que permite criar diagramas facilmente.",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+    ],
+  },
+
+  api: {
+    emoji: "⚙️",
+    data: [
+      {
+        nome: "Any API",
+        url: "https://any-api.com/",
+        descricao:
+          "Plataforma que oferece uma coleção de APIs para diversos fins, permitindo acesso fácil a uma variedade de serviços e funcionalidades.",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+      {
+        nome: "Repositório Public API",
+        url: "https://github.com/public-apis/public-apis",
+        descricao:
+          "Coleção de APIs gratuitas e de código aberto para desenvolvedores.",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+    ],
+  },
+
+  normalize_css: {
+    emoji: "⚙️",
+    data: [
+      {
+        nome: "Normalize.css",
+        url: "https://necolas.github.io/normalize.css/",
+        descricao:
+          "biblioteca que normaliza estilos entre diferentes navegadores, garantindo uma base consistente para o design web.",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+      {
+        nome: "Css Tolls: Reset",
+        url: "https://meyerweb.com/eric/tools/css/reset/",
+        descricao:
+          "O Reset CSS de Eric Meyer é uma ferramenta que oferece um estilo básico para zerar as configurações padrão dos navegadores, proporcionando um ponto de partida limpo e consistente para o desenvolvimento de CSS.",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+    ],
+  },
+
+  responsividade: {
+    emoji: "⚙️",
+    data: [
+      {
+        nome: "Responsive design checker",
+        url: "https://responsivedesignchecker.com/",
+        descricao:
+          "Permite visualizar facilmente como o seu site se adapta a diferentes dispositivos e tamanhos de tela. Teste a responsividade do seu site em tempo real e garanta uma experiência consistente para os usuários em todos os dispositivos.",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+      {
+        nome: "Am I Responsive?",
+        url: "https://ui.dev/amiresponsive",
+        descricao:
+          "Ferramenta simples que permite visualizar como o seu site aparece em diferentes tamanhos de tela, ajudando a garantir uma experiência consistente em dispositivos móveis, tablets e desktops.",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+    ],
+  },
+
+  acessibilidade: {
+    emoji: "⚙️",
+    data: [
+      {
+        nome: "Can I Use",
+        url: "https://caniuse.com/?search=HTML5",
+        descricao:
+          "Verifique a compatibilidade do HTML5 em diversos navegadores. Descubra facilmente quais recursos são suportados, facilitando o desenvolvimento web.",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+      {
+        nome: "Access Monitor",
+        url: "https://accessmonitor.acessibilidade.gov.pt/",
+        descricao:
+          "Ferramenta desenvolvida para avaliar a acessibilidade de sites de acordo com as diretrizes WCAG (Web Content Accessibility Guidelines).",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+    ],
+  },
+
+  performance: {
+    emoji: "⚙️",
+    data: [
+      {
+        nome: "1PageRank",
+        url: "https://1pagerank.com/",
+        descricao:
+          "Descubra o ranking de páginas web com facilidade. Obtenha insights rápidos sobre a classificação das páginas e otimize sua presença online de forma inteligente.",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+      {
+        nome: "FreeFormatter",
+        url: "https://www.freeformatter.com/html-formatter.html",
+        descricao:
+          "Torne o seu código mais legível e organizado com esta ferramenta online gratuita.",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+      {
+        nome: "Ahrefs Keyword Generator",
+        url: "https://ahrefs.com/pt/keyword-generator",
+        descricao:
+          "Poderosa ferramenta de pesquisa de palavras-chave que fornece sugestões relevantes e ideias de palavras-chave para impulsionar o seu SEO. Descubra termos-chave lucrativos e otimize o conteúdo do seu site para melhores resultados de pesquisa.",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+      {
+        nome: "CSS Lint",
+        url: "http://csslint.net/",
+        descricao:
+          "CSS Lint é uma ferramenta poderosa que analisa seu código CSS em busca de problemas de qualidade e conformidade com as melhores práticas. ",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+    ],
+  },
+
+  ideias: {
+    emoji: "⚙️",
+    data: [
+      {
+        nome: "BootstrapMade",
+        url: "https://bootstrapmade.com/",
+        descricao:
+          "Coleção diversificada de templates prontos para uso, projetados com Bootstrap, para acelerar o desenvolvimento web.",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+      {
+        nome: "Behance",
+        url: "https://www.behance.net/search/projects/sites%20web%20design?tracking_source=typeahead_search_direct",
+        descricao:
+          "Plataforma online que reúne uma ampla variedade de projetos de design, incluindo websites.",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+      {
+        nome: "Dribbble",
+        url: "https://dribbble.com/search/web-site",
+        descricao:
+          "Explore uma coleção inspiradora de designs de sites criativos, compartilhados por designers talentosos de todo o mundo.",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+      {
+        nome: "Mobbin",
+        url: "https://mobbin.com/browse/web/screens",
+        descricao:
+          "Plataforma que oferece uma ampla coleção de capturas de tela de designs de sites e aplicativos móveis.",
+        categorias: [
+          `https://th.bing.com/th/id/OIP.OesLvyzDO6AvU_hYUAT4IAHaHa?rs=1&pid=ImgDetMain`,
+        ],
+      },
+    ],
+  },
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  const elementosParaInserirLinks = document.getElementById("links");
+
+  exibirLinksNaTela(todosLinks);
+
+  function exibirLinksNaTela(listaDeLinks) {
+    for (const category in listaDeLinks) {
+      listaDeLinks[category].data.forEach((link) => {
+        let categoriasHTML = "";
+
+        link.categorias.forEach((categoria) => {
+          categoriasHTML += `<img class="card__categoria-image" src="${categoria}"/>`;
+        });
+
+        elementosParaInserirLinks.innerHTML += `
+        <div class="card">
+        <div>
+          <a class="card__link" href="${link.url}" target="_blank">
+            <h2 class="card__titulo">${link.nome}</h2>
+          </a>
+          <p class="card__descricao">${link.descricao}</p>
+          </div>
+          
+          <div class="card__categoria">${categoriasHTML}</div>
+        </div>
+        `;
+      });
+    }
+  }
+});
---
+++ src/scripts/teste.js
@@ -0,0 +1,330 @@
+const todosLinks = {
+  frameworks: {
+    emoji: "🛠️",
+    data: [
+      {
+        nome: "",
+        url: "",
+        descricao: "",
+        categorias: [``],
+      },
+    ],
+  },
+
+  bibliotecas: {
+    emoji: "📚",
+    data: [
+      {
+        nome: "",
+        url: "",
+        descricao: "",
+        categorias: [``],
+      },
+    ],
+  },
+
+  ferramentas: {
+    emoji: "⚙️",
+    data: [
+      {
+        nome: "TransferNow",
+        url: "https://www.transfernow.net/pt",
+        descricao:
+          "Plataforma online que permite enviar arquivos grandes de forma rápida e segura.",
+        categorias: [``],
+      },
+    ],
+  },
+
+  fontes: {
+    emoji: "⚙️",
+    data: [
+      {
+        nome: "1001 Fonts",
+        url: "https://www.1001fonts.com/",
+        descricao: "Ampla variedade de fontes gratuitas.",
+        categorias: [``],
+      },
+      {
+        nome: "Abstract Fonts",
+        url: "https://www.abstractfonts.com/",
+        descricao:
+          "Extensa coleção de fontes gratuitas para uso pessoal e comercial.",
+        categorias: [``],
+      },
+      {
+        nome: "FontGet",
+        url: "https://www.fontget.com/",
+        descricao:
+          "Plataforma para encontrar uma variedade de fontes para seus projetos de design.",
+        categorias: [``],
+      },
+      {
+        nome: "FontSpace",
+        url: "https://www.fontspace.com/",
+        descricao:
+          "Vasta coleção de fontes gratuitas, é um recurso valioso para designers gráficos, desenvolvedores web e qualquer pessoa que precise de fontes para uso em seus projetos criativos.",
+        categorias: [``],
+      },
+      {
+        nome: "BeFonts",
+        url: "https://befonts.com/",
+        descricao:
+          "Recurso útil para encontrar uma variedade de fontes para seus projetos",
+        categorias: [``],
+      },
+      {
+        nome: "UrbanFonts",
+        url: "https://www.urbanfonts.com/",
+        descricao:
+          "Ótima fonte para encontrar uma variedade de fontes para seus projetos criativos",
+        categorias: [``],
+      },
+      {
+        nome: "Netfontes",
+        url: "https://www.netfontes.com.br/",
+        descricao:
+          "Oferece uma ampla seleção de fontes gratuitas para designers gráficos e desenvolvedores web brasileiros.",
+        categorias: [``],
+      },
+      {
+        nome: "Adobe Fonts",
+        url: "https://fonts.adobe.com/fonts",
+        descricao:
+          "Disponibiliza uma vasta seleção de fontes para designers, com opções premium e gratuitas.",
+        categorias: [``],
+      },
+      {
+        nome: "FontPair",
+        url: "https://www.fontpair.co/",
+        descricao:
+          "Plataforma que oferece sugestões de combinações de fontes para projetos de design.",
+        categorias: [``],
+      },
+      {
+        nome: "Colors & Fonts",
+        url: "https://www.colorsandfonts.com/google-fonts/",
+        descricao:
+          "Ferramenta que oferece uma coleção organizada de fontes do Google Fonts.",
+        categorias: [``],
+      },
+      {
+        nome: "WhatFontIs",
+        url: "https://www.whatfontis.com/",
+        descricao:
+          "Serviço que permite identificar fontes em imagens, fornecendo opções semelhantes e informações detalhadas.",
+        categorias: [``],
+      },
+      {
+        nome: "MyFonts",
+        url: "https://www.myfonts.com/",
+        descricao:
+          "Plataforma onde você pode descobrir e comprar uma grande variedade de fontes para seus projetos de design.",
+        categorias: [``],
+      },
+      {
+        nome: "Google Fonts",
+        url: "https://fonts.google.com/",
+        descricao:
+          "Biblioteca online que oferece uma vasta coleção de fontes gratuitas para uso em projetos criativos.",
+        categorias: [``],
+      },
+      {
+        nome: "Font Squirrel",
+        url: "https://www.fontsquirrel.com/",
+        descricao:
+          "Fonte confiável para baixar fontes gratuitas e de alta qualidade para seus projetos de design.",
+        categorias: [``],
+      },
+      {
+        nome: "Dafont",
+        url: "https://www.dafont.com/pt/",
+        descricao:
+          "Popular site onde você pode encontrar uma grande variedade de fontes gratuitas para uso em seus projetos.",
+        categorias: [``],
+      },
+      {
+        nome: "W3BAI",
+        url: "https://www.w3bai.com/en-US/cssref/css_websafe_fonts.html#gsc.tab=0",
+        descricao:
+          "Fonte confiável para consultar informações sobre fontes web seguras e suas propriedades CSS.",
+        categorias: [``],
+      },
+      {
+        nome: "CSS Font Stack",
+        url: "https://www.cssfontstack.com/",
+        descricao:
+          "Referência útil que fornece uma lista de pilhas de fontes seguras para uso em CSS.",
+        categorias: [``],
+      },
+    ],
+  },
+
+  banco_de_dados: {
+    emoji: "⚙️",
+    data: [
+      {
+        nome: "SQL toad",
+        url: "https://sql.toad.cz/?",
+        descricao:
+          "Ferramenta online para converter consultas SQL entre diferentes dialetos, ajudando os desenvolvedores a trabalhar com SQL de forma mais eficiente.",
+        categorias: [``],
+      },
+      {
+        nome: "Excalidraw",
+        url: "https://excalidraw.com/",
+        descricao:
+          "Ferramenta de desenho colaborativa baseada na web, que permite criar diagramas facilmente.",
+        categorias: [``],
+      },
+    ],
+  },
+
+  api: {
+    emoji: "⚙️",
+    data: [
+      {
+        nome: "Any API",
+        url: "https://any-api.com/",
+        descricao:
+          "Plataforma que oferece uma coleção de APIs para diversos fins, permitindo acesso fácil a uma variedade de serviços e funcionalidades.",
+        categorias: [``],
+      },
+      {
+        nome: "Repositório Public API",
+        url: "https://github.com/public-apis/public-apis",
+        descricao:
+          "Coleção de APIs gratuitas e de código aberto para desenvolvedores.",
+        categorias: [``],
+      },
+    ],
+  },
+
+  normalize_css: {
+    emoji: "⚙️",
+    data: [
+      {
+        nome: "Normalize.css",
+        url: "https://necolas.github.io/normalize.css/",
+        descricao:
+          "biblioteca que normaliza estilos entre diferentes navegadores, garantindo uma base consistente para o design web.",
+        categorias: [``],
+      },
+      {
+        nome: "Css Tolls: Reset",
+        url: "https://meyerweb.com/eric/tools/css/reset/",
+        descricao:
+          "O Reset CSS de Eric Meyer é uma ferramenta que oferece um estilo básico para zerar as configurações padrão dos navegadores, proporcionando um ponto de partida limpo e consistente para o desenvolvimento de CSS.",
+        categorias: [``],
+      },
+    ],
+  },
+
+  responsividade: {
+    emoji: "⚙️",
+    data: [
+      {
+        nome: "Responsive design checker",
+        url: "https://responsivedesignchecker.com/",
+        descricao:
+          "Permite visualizar facilmente como o seu site se adapta a diferentes dispositivos e tamanhos de tela. Teste a responsividade do seu site em tempo real e garanta uma experiência consistente para os usuários em todos os dispositivos.",
+        categorias: [``],
+      },
+      {
+        nome: "Am I Responsive?",
+        url: "https://ui.dev/amiresponsive",
+        descricao:
+          "Ferramenta simples que permite visualizar como o seu site aparece em diferentes tamanhos de tela, ajudando a garantir uma experiência consistente em dispositivos móveis, tablets e desktops.",
+        categorias: [``],
+      },
+    ],
+  },
+
+  acessibilidade: {
+    emoji: "⚙️",
+    data: [
+      {
+        nome: "Can I Use",
+        url: "https://caniuse.com/?search=HTML5",
+        descricao:
+          "Verifique a compatibilidade do HTML5 em diversos navegadores. Descubra facilmente quais recursos são suportados, facilitando o desenvolvimento web.",
+        categorias: [``],
+      },
+      {
+        nome: "Access Monitor",
+        url: "https://accessmonitor.acessibilidade.gov.pt/",
+        descricao:
+          "Ferramenta desenvolvida para avaliar a acessibilidade de sites de acordo com as diretrizes WCAG (Web Content Accessibility Guidelines).",
+        categorias: [``],
+      },
+    ],
+  },
+
+  performance: {
+    emoji: "⚙️",
+    data: [
+      {
+        nome: "1PageRank",
+        url: "https://1pagerank.com/",
+        descricao:
+          "Descubra o ranking de páginas web com facilidade. Obtenha insights rápidos sobre a classificação das páginas e otimize sua presença online de forma inteligente.",
+        categorias: [``],
+      },
+      {
+        nome: "FreeFormatter",
+        url: "https://www.freeformatter.com/html-formatter.html",
+        descricao:
+          "Torne o seu código mais legível e organizado com esta ferramenta online gratuita.",
+        categorias: [``],
+      },
+      {
+        nome: "Ahrefs Keyword Generator",
+        url: "https://ahrefs.com/pt/keyword-generator",
+        descricao:
+          "Poderosa ferramenta de pesquisa de palavras-chave que fornece sugestões relevantes e ideias de palavras-chave para impulsionar o seu SEO. Descubra termos-chave lucrativos e otimize o conteúdo do seu site para melhores resultados de pesquisa.",
+        categorias: [``],
+      },
+      {
+        nome: "CSS Lint",
+        url: "http://csslint.net/",
+        descricao:
+          "CSS Lint é uma ferramenta poderosa que analisa seu código CSS em busca de problemas de qualidade e conformidade com as melhores práticas. ",
+        categorias: [``],
+      },
+    ],
+  },
+
+  ideias: {
+    emoji: "⚙️",
+    data: [
+      {
+        nome: "BootstrapMade",
+        url: "https://bootstrapmade.com/",
+        descricao:
+          "Coleção diversificada de templates prontos para uso, projetados com Bootstrap, para acelerar o desenvolvimento web.",
+        categorias: [``],
+      },
+      {
+        nome: "Behance",
+        url: "https://www.behance.net/search/projects/sites%20web%20design?tracking_source=typeahead_search_direct",
+        descricao:
+          "Plataforma online que reúne uma ampla variedade de projetos de design, incluindo websites.",
+        categorias: [``],
+      },
+      {
+        nome: "Dribbble",
+        url: "https://dribbble.com/search/web-site",
+        descricao:
+          "Explore uma coleção inspiradora de designs de sites criativos, compartilhados por designers talentosos de todo o mundo.",
+        categorias: [``],
+      },
+      {
+        nome: "Mobbin",
+        url: "https://mobbin.com/browse/web/screens",
+        descricao:
+          "Plataforma que oferece uma ampla coleção de capturas de tela de designs de sites e aplicativos móveis.",
+        categorias: [``],
+      },
+    ],
+  },
+};
